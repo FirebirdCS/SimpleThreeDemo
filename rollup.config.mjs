12999929c6f63f3ad0d5550c31a99c187f8716ed
FirebirdCS <alvaritotutos@gmail.com>
2024-11-28 11:49:58 -0600

    Build fixed

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,12 @@
+import resolve from "@rollup/plugin-node-resolve";
+
 export default {
   input: "index.js",
-  output: {
-    file: "bundle.js",
-  },
+  output: [
+    {
+      format: "esm",
+      file: "bundle.js",
+    },
+  ],
+  plugins: [resolve()],
 };
